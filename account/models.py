d8176c64216f46a0340030d6b1bf8960c32be82e
Marvin Kweyu <mkweyu1@gmail.com>
2022-11-21 17:53:57 +0300

    chore: add account model for contact

--- account/models.py
+++ account/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 
 class Profile(models.Model):
+    """User profile"""
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
@@ -10,3 +11,18 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profile for {self.user.username}"
+
+
+class Contact(models.Model):
+    """Hold relationship for follower accounts"""
+    user_to = models.ForeignKey('auth.User', related_name="rel_from_set" on_delete=models.CASCADE)
+    user_from = models.ForeignKey(
+        'auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
+    # improve queries ordering results by this field
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self) -> str:
+        return f"{self.user_from} follows {self.user_to}"
