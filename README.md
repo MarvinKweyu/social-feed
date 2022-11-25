47880d638cd9bfe480d52c634a65a0083c9d4354
Marvin Kweyu <mkweyu1@gmail.com>
2022-11-25 10:22:29 +0300

    feat: add image ranking
    docs: update readme set up guide

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # SocialFeed
 
-> An image-sharing social application
+![Python](https://img.shields.io/badge/Python-3.10-green?style=flat)
+
+> An image-bookmarking social application
 
 
 ## Bare metal setup
 
-Modify your `/etc/hosts` file and adjust it as below:
+This project requires that you have [redis](https://redis.io/) installed and running on your host machine.
+After the install, modify your `/etc/hosts` file and adjust it as below:
 
 ```bash
+# /etc/hosts
 127.0.0.1 socialfeed.com
 ```
 
@@ -18,7 +22,9 @@ python manage.py migrate
 python manage.py runserver_plus --cert-file cert.crt
 ```
 
-## It just works(Docker setup)
+## It just works(Docker setup) - Work in progress
+
+You can get the project up and running out of hte box and ready for development or production as below.
 
 **Development**
 
@@ -38,5 +44,6 @@ docker-compose -f docker-compose.prod.yml up -d --build
 > Access the project via: **127.0.0.1:8000/account** or **socialfeed.com:8000/account**
 
 
+### Note
 For easy use of this project,create a bookmark on the *bookmark* bar of your browser by dragging the button at the *account* page.
 Visit any site and click on the bookmark to select the image to bookmark. Happy tagging!
--- images/views.py
+++ images/views.py
@@ -46,6 +46,7 @@ def image_detail(request, id, slug):
     # increment view count by one or create it if non-existent
     # * convention object-type:id:field thus in this case image:2:views
     total_views = r.incr(f'image:{image.id}:views')
+    r.zincrby('image_ranking', 1, image.id)
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image, 'total_views': total_views})
 
 
@@ -87,3 +88,17 @@ def image_list(request):
     if is_ajax(request=request):
         return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # image rank dictionary. Return all elements in sorted set then get top 10
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # most viewed
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,
+                  'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
